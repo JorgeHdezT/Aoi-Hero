05dad369b335f4b3ebdf4b1e948eedba84e471f7
Jorge Hernandez Toledo <jorgeht2001@gmail.com>
2024-01-30 10:17:42 +0000

    Script otra vez
    Madre mia, todo errores por el fetch

--- js/nightMode.js
+++ js/nightMode.js
@@ -12,31 +12,34 @@ const modoNocheBtn = document.getElementById("checkbox");
 
     modoNocheBtn.addEventListener("click", () => {
         body.classList.toggle("modo-noche");
-        if (body.classList.contains("modo-noche")) {
-        modoNocheBtn.textContent = "Modo Día";
-        modoNocheBtn.className = "modo-noche";
-        body.style.backgroundColor = "#0a2540";
-        body.style.color = "#fff";
-        header.style.backgroundColor = "#0052a3";
-        navBar_movil.style.backdropfilter = "blur(10%)";
-        navBar_movil.style.backgroundColor = "transparent";
+        if (body.classList.contains("modo-noche"))
+        {
+            modoNocheBtn.textContent = "Modo Día";
+            modoNocheBtn.className = "modo-noche";
+            body.style.backgroundColor = "#0a2540";
+            body.style.color = "#fff";
+            header.style.backgroundColor = "#0052a3";
+            navBar_movil.style.backdropfilter = "blur(10%)";
+            navBar_movil.style.backgroundColor = "transparent";
 
-        h2.forEach(element => {
-        element.style.color = "#fff";
-        });
+            h2.forEach(element => {
+            element.style.color = "#fff";
+            });
     
         } else {
-        modoNocheBtn.textContent = "Modo Noche";
-        modoNocheBtn.className = "modo-dia";
-        body.style.backgroundColor = "";
-        body.style.color = "";
-        header.style.backgroundColor = "";
-        navBar_movil.style.backdropfilter = "";
-        navBar_movil.style.backgroundColor = "";
+            modoNocheBtn.textContent = "Modo Noche";
+            modoNocheBtn.className = "modo-dia";
+            body.style.backgroundColor = "";
+            body.style.color = "";
+            header.style.backgroundColor = "";
+            navBar_movil.style.backdropfilter = "";
+            navBar_movil.style.backgroundColor = "";
 
-        h2.forEach(element => {
-            element.style.color = "#000";
-        });
+            h2.forEach(element => {
+                element.style.color = "#000";
+            });
+
+        }
 });
 
 
